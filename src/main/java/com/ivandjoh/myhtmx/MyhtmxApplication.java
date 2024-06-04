aef93a6b2f078aed5027f7dfc5720cbaff6a4458
idsta.ivandi <idsta.ivandi@xl.co.id>
2024-06-05 07:00:21 +0800

    first commit

---
+++ src/main/java/com/ivandjoh/myhtmx/MyhtmxApplication.java
@@ -0,0 +1,13 @@
+package com.ivandjoh.myhtmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhtmxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyhtmxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivandjoh/myhtmx/MyhtmxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivandjoh.myhtmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhtmxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
